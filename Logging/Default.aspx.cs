b8dbdcd403b28d7fa020343a1179d549a109e0d7
sarthak-k <sarthak.killedar@outlook.com>
2014-08-08 11:35:18 +0530

    Initialize Project
    First commit - Initialization of Project.

---
+++ Logging/Default.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Microsoft.Practices.EnterpriseLibrary.Logging;
+using Microsoft.Practices.EnterpriseLibrary.ExceptionHandling;
+using Microsoft.Practices.EnterpriseLibrary.Common.Configuration;
+
+
+namespace Logging
+{
+    public partial class Default : System.Web.UI.Page
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                int a=7;
+                int b= 0;
+                int c = a/b;
+            }
+            catch (Exception ex)
+            {
+                ExceptionPolicy.HandleException(ex, "LoggingPolicy");
+            }
+        }
+    }
+}
